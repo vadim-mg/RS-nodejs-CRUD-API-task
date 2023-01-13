59fc40fd821545b4261b8de78d692b59639320e1
vadim-mg <vadikm1981@yandex.ru>
2023-01-13 20:28:33 +0500

    feat: implement app.ts for supertest

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import { createServer } from 'node:http'
+import { router } from './routers/router.js'
+import { errorController } from './controllers/error-controller.js';
+
+
+const server = createServer();
+
+server.on('request', async (req, res) => {
+  console.log(`---------------------------------------------------`)
+  try {
+    console.log(`Request: (${req.method})http:...${req.url}`)
+    await router(req, res)
+  } catch (e: any) {
+    errorController(res, e)
+  }
+  console.log(`Response statusCode: ${res.statusCode}`)
+});
+
+export { server }
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,5 @@
+import { server } from './app.js'
 import { DOMAIN, PORT } from './utils/constants.js';
-import { createServer } from 'node:http'
-import { router } from './routers/router.js'
-import { errorController } from './controllers/error-controller.js';
 
 
-const server = createServer();
-
-server.on('request', async (req, res) => {
-  try {
-    console.log(`---------------------------------------------------`)
-    console.log(`Request: (${req.method})${DOMAIN}:${PORT}${req.url}`)
-    await router(req, res)
-    console.log(`Response statusCode: ${res.statusCode}`)
-  } catch (e: any) {
-    errorController(res, e)
-  }
-});
-
-
-server.listen(PORT, () => console.log(`Started server on http:\/\/localhost:${PORT}/`));
+server.listen(PORT, () => console.log(`Started server on http:\/\/${DOMAIN}:${PORT}/`));
